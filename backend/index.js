237c50166a365f0cec61ab796154ca91fc5e07e5
Anurag Uniyal <105287057+Anuraguni35@users.noreply.github.com>
2023-06-04 20:29:26 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
 const express=require('express');
 // const PORT=8082;
 require('dotenv').config();
+const cors = require("cors");
 const dotenv=require('dotenv');
 const app=express();
 const mongoose=require('mongoose');
 const router=require('./Routes/index.routes');
 // to enable the json 
 app.use(express.json());
-
+app.use(cors());
+app.options("*", cors());
 
 app.use('/v1',router);
 
